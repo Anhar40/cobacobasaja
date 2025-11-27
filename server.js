2e56f4c3ccb37fa80604b05f5472d5febd07d234
mbojostudio <anharaldevaro789@gmail.com>
2025-11-27 16:55:29 +0800

    update

--- server.js
+++ server.js
@@ -80,7 +80,21 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/tentang", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "about.html"));
+});
 
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "register.html"));
+});
+
+app.get("/eporner", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "eporner.html"));
+});
 
 // ---------------------------------------------------
 // 4. Server
